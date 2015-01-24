1ce2b2e2d30cce6411f028bf79fd1e4942cb4ae4
wiebesella <wiebesella@gmail.com>
2015-01-24 20:10:44 +0100

    test

--- app/src/main/java/com/app/siwigi/mywordtrain/MyWordTrain.java
+++ app/src/main/java/com/app/siwigi/mywordtrain/MyWordTrain.java
@@ -18,7 +18,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
+/**
+ * My First Android Studio App
+ */
 public class MyWordTrain extends ActionBarActivity {
 
     /**
